7d57da1ae4f107abcf6b079f63c6e1298b66dbf1
Ocean Hart <pezechigozirim@gmail.com>
2020-04-03 02:06:48 +0100

    changing title

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Maths game</title>
+	<title>Fun Maths Game</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width = device-width, initial-scale = 1, user-scalable=yes">
 	<link rel="stylesheet" type="text/css" href="style.css">
